b38f9c26bb8b925a8e4285ed045cb693806a8b7d
tiagoluzh607 <tiagoluz.h607@gmail.com>
2018-07-17 15:01:37 -0300

    Incluindo uma funcao para limitar numero de caracteres apresentados ao usuario na view, fazendo uso do metodo onInit de component

--- client/app/painel/painel.component.ts
+++ client/app/painel/painel.component.ts
@@ -1,11 +1,16 @@
-import {Component, Input} from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 
 @Component({
     moduleId: module.id,
     selector: 'painel',
     templateUrl: './painel.component.html'
 })
-export class PainelComponent{
+export class PainelComponent implements OnInit{
     
     @Input() titulo : String;
+    
+    //Essa funcao vem do implements oninit e é executada assim que recebemos os nossos atributos input
+    ngOnInit(){
+        this.titulo = this.titulo.length > 7 ? this.titulo.substring(0,7) + '...' : this.titulo;
+    }
 }
