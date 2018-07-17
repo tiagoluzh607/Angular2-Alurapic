bc3ff4046ff9513ad1ef898fe8eb4397814f435e
tiagoluzh607 <tiagoluz.h607@gmail.com>
2018-07-16 22:48:48 -0300

    Fazendo o componente foto receber parametros como url e titulo

--- client/app/foto/foto.component.ts
+++ client/app/foto/foto.component.ts
@@ -1,8 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, Input} from '@angular/core';
 
 @Component({
     moduleId: module.id,
     selector: 'foto',
     templateUrl: './foto.component.html'
 })
-export class FotoComponent{}
+export class FotoComponent{
+    
+    @Input() titulo;
+    @Input() url;
+    
+}
