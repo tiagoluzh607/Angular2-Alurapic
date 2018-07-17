0f231189933a66a91a0fbcc89cb5031122e54de9
tiagoluzh607 <tiagoluz.h607@gmail.com>
2018-07-17 13:06:30 -0300

    Filtrando a listagem com um campo de pesquisa

--- client/app/foto/foto.module.ts
+++ client/app/foto/foto.module.ts
@@ -1,9 +1,10 @@
 import{NgModule} from '@angular/core';
 import {FotoComponent} from './foto.component';
+import {FiltroPorTitulo} from './foto.pipes';
 
 @NgModule({
-    declarations: [ FotoComponent ], //tuod que faz parte do modulo
-    exports: [FotoComponent] //tudo que eu quero que seja possivel usar de outros modulos
+    declarations: [FotoComponent, FiltroPorTitulo ], //tuod que faz parte do modulo
+    exports: [FotoComponent, FiltroPorTitulo] //tudo que eu quero que seja possivel usar de outros modulos
 })
 export class FotoModule{
     
---
+++ client/app/foto/foto.pipes.ts
@@ -0,0 +1,15 @@
+import {Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+    name: 'filtroPorTitulo'
+})
+export class FiltroPorTitulo implements PipeTransform{
+    
+    transform(fotos, digitado){
+        
+        digitado = digitado.toLowerCase();
+        return fotos.filter(foto => foto.titulo.toLowerCase().includes(digitado));
+    }
+    
+}
+
